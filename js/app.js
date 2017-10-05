6c0871293811cd69ee249fb76e31d5d1681299d5
Parul kumar <prlkumar96@gmail.com>
2017-10-05 17:27:11 +0530

    add cards and onclick method

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 /*
  * Create a list that holds all of your cards
  */
+var cardList = ["diamond", "paper-plane-o", "anchor", "bolt",
+
+	"cube", "bomb", "bicycle", "leaf",
+
+	"diamond", "paper-plane-o", "anchor", "bolt",
+
+	"cube", "bomb", "bicycle", "leaf",
+];
 
 
 /*
@@ -9,20 +17,29 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+cardList = shuffle(cardList);
+
+for (var i = 0; i < 16; i++) {
+	var HTML = '<li class="card" id = "' + cardList[i] + '"><i class="fa fa-' + cardList[i] + '"></i></li>';
+	$('.deck').append(HTML);
+}
+
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length,
+		temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
 
 
@@ -36,3 +53,27 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+var openedCards = [];
+
+function display(el) {
+	console.log(el.id);
+	$(el).addClass("show open");
+}
+
+function addToOpen(el) {
+	openedCards.push(el.id);
+}
+
+function hideCard(cardId) {
+	$("#" + cardId).toggleClass("show open");
+}
+$('.card').click(function () {
+	display(this);
+	addToOpen(this);
+	if (openedCards.length > 1) {
+		if (openedCards[openedCards.length - 2] !== openedCards[openedCards.length - 1]) {
+			hideCard(openedCards.pop());
+			hideCard(openedCards.pop());
+		}
+	}
+});
